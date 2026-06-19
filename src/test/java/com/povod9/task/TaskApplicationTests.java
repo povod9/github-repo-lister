db6364b44d4809dcfd30043649b270442f187c6e
povod9 <nazarpovod113@gmail.com>
2026-06-19 11:07:28 +0200

    init commit

---
+++ src/test/java/com/povod9/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.povod9.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
